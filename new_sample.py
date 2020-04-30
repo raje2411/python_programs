59ae07b1749eeb7a5ef4d766aeb054dcbfdae31a
Rajesh Raman <raje2411@gmail.com>
2020-04-30 10:34:42 -0400

    second mod

--- new_sample.py
+++ new_sample.py
@@ -2,7 +2,7 @@
 first_name = "rajesh"
 last_name = "raman"
 age = int(55)
-birth_date = 01-Dec-1966"
+birth_date = "01-Dec-1966"
 
 print("My name is %s %s" %(first_name, last_name))
 print("I was born on %s, and I'm %s years old" %(birth_date,age))
